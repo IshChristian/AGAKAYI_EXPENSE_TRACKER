939e4089aefd3c17af4702b692fb7f6e9f0308e5
IshChristian <tiangroupinnovation@gmail.com>
2025-06-01 23:23:35 +0200

    >>>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   output: 'export',
-  // Disable trailing slashes since you have no routes
   trailingSlash: false,
-  // If using images
-  images: {
-    unoptimized: true
+  images: { unoptimized: true },
+  // Critical for SPAs:
+  experimental: {
+    missingSuspenseWithCSRBailout: false
   }
 }
