e2fc640e602fa2d75a6922c3030696b2dcc5ebda
IshChristian <tiangroupinnovation@gmail.com>
2025-06-01 23:11:55 +0200

    >>>

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  output: 'export',
+  // Disable trailing slashes since you have no routes
+  trailingSlash: false,
+  // If using images
+  images: {
+    unoptimized: true
+  }
+}
