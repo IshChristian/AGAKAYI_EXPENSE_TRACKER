9f5a892fe8101292412223372b41742f31667704
IshChristian <tiangroupinnovation@gmail.com>
2025-06-03 17:33:21 +0200

    V1.0.0.0

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ import Dashboard from "@/components/Dashboard"
 export default function Home() {
   const { user } = useAuth()
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
-  //       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-600"></div>
-  //     </div>
-  //   )
-  // }
+   if (isLoading) {
+    return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-600"></div>
+      </div>
+     )
+  }
 
   return user ? <Dashboard /> : <AuthPage />
 }
